0d1cbc4458ac2d6777f99f22978c22bdce3be5bf
ray <31711787@qq.com>
2018-08-24 20:10:07 +0800

    2nd commit

--- muju/config.php
+++ muju/config.php
@@ -11,6 +11,7 @@
     array (
       'category.html' => '文章频道页',
       'category_download.html' => '下载频道页',
+	  'category_news.html' => '新闻频道页',
       'category_picture.html' => '图片频道页',
       'download.html' => '下载连接页',
       'footer.html' => '底部',
@@ -20,6 +21,7 @@
       'header_page.html' => '单网页头部',
       'index.html' => '网站首页',
       'list.html' => '列表页',
+	   'list_kehu.html' => '客户见证列表页',
       'list_download.html' => '下载列表页',
       'list_picture.html' => '图片列表页',
       'message.html' => '消息提示页',
